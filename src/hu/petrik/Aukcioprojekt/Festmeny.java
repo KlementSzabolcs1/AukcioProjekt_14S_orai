e5756718791a6666fb433264d000787e2d3cd276
Krisztián <radeczki.krisztian@gmail.com>
2022-09-29 18:38:45 +0200

    Itt fejeztük be

--- src/hu/petrik/Aukcioprojekt/Festmeny.java
+++ src/hu/petrik/Aukcioprojekt/Festmeny.java
@@ -2,6 +2,7 @@ package hu.petrik.Aukcioprojekt;
 
 import java.nio.BufferOverflowException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Festmeny {
     public static final int KEZDETI_LICIT = 100;
@@ -19,7 +20,7 @@ public class Festmeny {
         this.festo = festo;
         this.stilus = stilus;
         this.licitekszama = 0;
-        this.legmagasabbLicit =0;
+        this.legmagasabbLicit = 0;
         this.legutolsoLicitIdeje = null;
         this.elkelt = false;
 
@@ -59,43 +60,69 @@ public class Festmeny {
         this.elkelt = elkelt;
     }
 
-    public void licit(){
-        if (this.elkelt){
+    public void licit() {
+        this.licit(10);
+
+    }
+
+    public void licit(int mertek) {
+        if (mertek < 10 || mertek > 100) {
+            throw new IllegalArgumentException("A lciti mértéke 10 és 100 % közé kell hogy essen");
+        }
+        if (this.elkelt) {
             throw new RuntimeException("A festmeny már elkelt");
         }
-        if (this.licitekszama == 0){
+        if (this.licitekszama == 0) {
             this.legmagasabbLicit = KEZDETI_LICIT;
-        }else {
-            int ujLicit = (int)(this.legmagasabbLicit *1.1);
+        } else {
+            int ujLicit = (int) (this.legmagasabbLicit * (1 + mertek / 100));
             this.legmagasabbLicit = getVeglegesLicitStringreAlakitassal(ujLicit);
         }
         this.licitekszama++;
         this.legutolsoLicitIdeje = LocalDateTime.now();
-
+        //TODO: Majd megcsináljuk.
     }
 
-    private int getVeglegesLicitMatematikaimuveletekkel(int ujLicit){
+    private int getVeglegesLicitMatematikaimuveletekkel(int ujLicit) {
         int ossztasokszama = 0;
-        while (ujLicit > 100){
+        while (ujLicit > 100) {
             ujLicit /= 10;
             ossztasokszama++;
         }
-        return (int)(ujLicit * Math.pow(10, ossztasokszama));
+        return (int) (ujLicit * Math.pow(10, ossztasokszama));
     }
 
 
     private int getVeglegesLicitStringreAlakitassal(int ujLicit) {
         String szovegesLicit = String.valueOf(ujLicit);
         int hossz = szovegesLicit.length();
-        StringBuilder builder = new StringBuilder(szovegesLicit.substring(0,2));
-        for (int i = 0; i < hossz - 2 ; i++) {
+        StringBuilder builder = new StringBuilder(szovegesLicit.substring(0, 2));
+        for (int i = 0; i < hossz - 2; i++) {
             builder.append(0);
         }
         return Integer.parseInt(builder.toString());
     }
 
-    public void licit(int mertek){
 
-        //TODO: Majd megcsináljuk.
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String festmenyAdatai = String.format("%s: %s (%s)",
+                this.festo, this.cim, this.stilus);
+        if (this.licitekszama > 0) {
+            String licitAdatai = String.format("%s: %s (%s)\n" +
+                            "%s\n" +
+                            "%d $ - %s (összesen: %d db)\n",
+                    this.festo, this.cim, this.stilus,
+                    (this.elkelt) ? "elkelt" : "nem kelt",
+                    this.legmagasabbLicit, this.legutolsoLicitIdeje, this.licitekszama);
+
+            festmenyAdatai += "\n" + licitAdatai;
+        }else {
+            festmenyAdatai += "\nMég nem érkezett licit";
+        }
+
+        return festmenyAdatai;
+
     }
 }
--- src/hu/petrik/Aukcioprojekt/Main.java
+++ src/hu/petrik/Aukcioprojekt/Main.java
@@ -1,11 +1,120 @@
 package hu.petrik.Aukcioprojekt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        Festmeny festmeny = new Festmeny("Uj festmeny","Kiki","futurisztikus");
-        for (int i = 0; i < 100; i++) {
-            festmeny.licit();
-            System.out.println(festmeny.getLegmagasabbLicit());
+    static List<Festmeny> festmenyek = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        festmenyekHozzaadasa();
+        try{
+            festmenyekFelveteleKonzolal();
+        } catch (RuntimeException e){
+            System.out.println("A darabszám csak a természetes számlehet");
+        }
+        String fajlNev = "festmenyek.csv";
+        try {
+            festmenyekFelveteleSzovegesAllomanybol(fajlNev);
+        } catch (FileNotFoundException e) {
+            System.out.println("Hiba! nem talalhato az alabbi fajl");
+        } catch (IOException e){
+            System.out.println("Ismeretlen Hiba történt a forrás fájl ovasása után");
+            System.out.println(e.getMessage());
+        }
+        veletlenszeruLicit();
+        try {
+            konzolosLicitalas();
+        }catch (NumberFormatException e){
+            System.out.println("Nem sorszámot adott meg");
+        }
+
+
+        for (Festmeny festmeny : festmenyek) {
+
+
         }
     }
+
+    private static void konzolosLicitalas() {
+        System.out.println("Kérem adja meg a festmény sorszámát (kilépéshez adjon 0-t :)");
+        int sorszam = -1;
+                while (sorszam != 0){
+                    sorszam = Integer.parseInt(sc.nextLine());
+                }
+        if (sorszam < 0){
+            throw new NumberFormatException();
+        }
+        if (sorszam > festmenyek.size()){
+            System.out.println("A listában nincs ennyi elem., a lista elemszáma: " + festmenyek.size());
+        } else {
+            Festmeny festmeny = festmenyek.get(sorszam -1);
+            if(festmeny.isElkelt()){
+                System.out.println("Ez a festmény elkellt");
+            }else {
+                System.out.println("Kérem adja meg, hogy milyen mértékkel szeretne licitálni (10-100%): ");
+                String mertek = sc.nextLine();
+                if (mertek.isEmpty()){
+                    festmeny.licit();
+                }else {
+                    festmeny.licit(Integer.parseInt(mertek));
+                }
+            }
+        }
+    }
+
+    private static void veletlenszeruLicit() {
+        for (int i = 0; i < 20; i++) {
+            int index =(int) (Math.random() * festmenyek.size());
+            festmenyek.get(index).licit();
+        }
+    }
+
+    private static void festmenyekFelveteleSzovegesAllomanybol(String fajlNev) throws IOException {
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+            String sor = br.readLine();
+            while (sor != null && !sor.isEmpty()){
+                String[] adatok = sor.split(";");
+                Festmeny festmeny = new Festmeny(adatok[0], adatok[1], adatok[2]);
+                festmenyek.add(festmeny);
+                sor = br.readLine();
+            }
+        br.close();
+        fr.close();
+    }
+
+    private static void festmenyekFelveteleKonzolal(){
+        System.out.println("Kérem adjon meg, hogy hány festményt szeretne felvenni:");
+        int db = Integer.parseInt(sc.nextLine());
+        if(db < 0){
+            throw new NumberFormatException("");
+        }
+        for (int i = 0; i < db ; i++){
+            System.out.printf("Kérem adjon meg a(z) %d fetmény címét: \n",(i + 1));
+            String cim = sc.nextLine();
+            System.out.printf("Kérem adjon meg a(z) %d fetmény festőjét: \n",(i + 1));
+            String festo = sc.nextLine();
+            System.out.printf("Kérem adjon meg a(z) %d fetmény stilusat: \n",(i + 1));
+            String stilus = sc.nextLine();
+            Festmeny festmeny = new Festmeny(cim,festo,stilus);
+            festmenyek.add(festmeny);
+        }
+    }
+
+    private static void festmenyekHozzaadasa(){
+        Festmeny festmeny = new Festmeny("Uj festmeny","Kiki","futurisztikus");
+        Festmeny festmeny2 = new Festmeny("Masik festmeny","Kiki","futurisztikus");
+        festmenyek.add(festmeny);
+        festmenyek.add(festmeny2);
+    }
 }
+
+
+
